2b5027107b87a0f14260b33b9f596de442e4fd44
MaximoFC <maximofcallejas@gmail.com>
2024-04-27 22:13:38 -0300

    change router for app - post solves

--- app.js
+++ app.js
@@ -1,39 +1,39 @@
-const { request } = require('express');
 const express = require('express');
 const dbconnect = require('./config');
 const modelUser = require('./userModel');
 const app = express();
 const router = express.Router();
 
-app.use(router);
+//app.use(router);
 app.use(express.json());
+app.use(require("body-parser").json());
 
 //CRUD - Create, Read, Update, Delete
-router.post("/", async (req, res)=>{
+app.post("/", async (req, res)=>{
     const body = req.body;
     const response = await modelUser.create(body);
     res.send(response);
 })
 
-router.get("/", async (req, res)=>{
+app.get("/", async (req, res)=>{
     const response = await modelUser.find({});
     res.send(response);
 })
 
-router.get("/:id", async (req, res)=>{
+app.get("/:id", async (req, res)=>{
     const id = req.params.id;
     const response = await modelUser.findById(id);
     res.send(response);
 })
 
-router.put("/:id", async (req, res)=>{
+app.put("/:id", async (req, res)=>{
     const body = req.body;
     const id = req.params.id;
     const response = await modelUser.findOneAndUpdate({_id:id}, body);
     res.send(response);
 })
 
-router.delete("/:id", async (req, res)=>{
+app.delete("/:id", async (req, res)=>{
     const id = req.params.id;
     const response = await modelUser.deleteOne({_id:id});
     res.send(response);
